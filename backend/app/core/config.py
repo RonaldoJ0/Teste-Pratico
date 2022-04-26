5ae7ade1eb2e506a6a42f87ac46cf9cc07dd611b
RonaldoJ0 <ronaldonascimento977@gmail.com>
2022-04-26 00:47:28 -0400

    fix: Corrige caminho do banco de dados.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,19 @@
-from pydantic import BaseSettings
+from typing import Optional, Dict, Any
+from pydantic import BaseSettings, validator
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    PROJECT_NAME: str 
-    SQLALCHEMY_DATABASE_URI: str
+    PROJECT_NAME: str
+    SCHEME_DATABASE: str
+    DATABASE_NAME: str
+    SQLALCHEMY_DATABASE_URI: Optional[str] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return values.get("SCHEME_DATABASE") + values.get("DATABASE_NAME")
 
     class Config:
         case_sensitive = True
